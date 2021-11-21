a16f2750fce86631818254dabfbb738537bea030
kisihara.c <kisihara.c@gmail.com>
2021-11-21 19:29:43 +0900

    write first test and comment out

--- test/controllers/mailrs_controller_test.rb
+++ test/controllers/mailrs_controller_test.rb
@@ -4,4 +4,16 @@ class MailrsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  #test "ins_normal" do
+  #  mc = MailrsController.new
+  #  dt = DateTime.new(2012,1,1,1,1,1)
+  #  mc.hash_ins({
+  #      destAddress:'test@example.com',sendingAtYe:'2000',sendingAtMo:'1',sendingAtDa:'1',
+  #      sendingAtHo:'1',sendingAtMi:'1',remindStr:'test'
+  #    })
+#
+  #  p Mailr.last
+  #  assert(Mailr.last.dest_address == 'test@example.com')
+#
+  #end
 end
