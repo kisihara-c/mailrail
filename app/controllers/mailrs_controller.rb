75298b9dccae75aabe3c48121472023cbb5f6f38
kisihara.c <kisihara.c@gmail.com>
2021-11-28 20:10:46 +0900

    make simple validation

--- app/controllers/mailrs_controller.rb
+++ app/controllers/mailrs_controller.rb
@@ -10,11 +10,19 @@ class MailrsController < ApplicationController
         jrs = JSON.parse(request.body.read, {:symbolize_names => true})
         p "json_request => #{jrs}"
 
+        jrs = json_shape(jrs)
+
+        if json_val_alert(jrs)
+            ret = '{"r":"おや？　入力がおかしいようです…"}'
+            render json: JSON.parse(ret)
+            return
+        end
+
         resultBool = hash_ins(jrs)
         if resultBool
             render json: @mailr.inspect.to_json
         else
-            ret = {"r":"おや？　何かがおかしいようです…"}
+            ret = {"r"=>"おや？　何かがおかしいようです…"}
             render json: ret.to_json
         end
 
@@ -32,6 +40,42 @@ class MailrsController < ApplicationController
             return false
         end
 
+    end
+    
+    # 今は空
+    def json_shape(jrs)
+        return jrs
+    end
+
+    def json_val_alert(jrs)
+
+        if jrs[:sendingAtMo] == "2"
+            if jrs[:sendingAtDa] == "30" || jrs[:sendingAtDa] == "31"
+                return true
+            end
+        end
+
+        if jrs[:sendingAtMo] == "2" && jrs[:sendingAtDa] == "29"
+            y = jrs[:sendingAtYe].to_i
+
+            if y % 4 != 0
+                return true
+            end
+
+            if y % 100 == 0 && y % 400 != 0
+                return true
+            end
+            
+        end
+
+        if jrs[:sendingAtMo] == "4" || jrs[:sendingAtMo] == "6"  || jrs[:sendingAtMo] == "9" 
+            if jrs[:sendingAtDa] == "31"
+                return true
+            end
+        end
+
+        # 問題なければ常にfalse
+        return false
     end
 
     def create
--- test/controllers/mailrs_controller_test.rb
+++ test/controllers/mailrs_controller_test.rb
@@ -15,4 +15,75 @@ class MailrsControllerTest < ActionDispatch::IntegrationTest
     assert(Mailr.last.remind_str == 'test')
 
   end
+  
+  test "json_val_alert_normal" do
+
+    mc = MailrsController.new
+    res = mc.json_val_alert({
+        sendingAtYe:'2000',sendingAtMo:'1',sendingAtDa:'1'
+      })
+
+    assert(res == false)
+
+  end
+  
+  test "json_val_alert_exeption_day30-31" do
+
+    mc = MailrsController.new
+
+    res = mc.json_val_alert({
+        sendingAtYe:'2000',sendingAtMo:'2',sendingAtDa:'30'
+      })
+    assert(res == true)
+    
+    res = mc.json_val_alert({
+        sendingAtYe:'2000',sendingAtMo:'2',sendingAtDa:'31'
+      })
+    assert(res == true)
+    
+    res = mc.json_val_alert({
+        sendingAtYe:'2000',sendingAtMo:'4',sendingAtDa:'31'
+      })
+    assert(res == true)
+    
+    res = mc.json_val_alert({
+        sendingAtYe:'2000',sendingAtMo:'6',sendingAtDa:'31'
+      })
+    assert(res == true)
+    
+    res = mc.json_val_alert({
+        sendingAtYe:'2000',sendingAtMo:'9',sendingAtDa:'31'
+      })
+    assert(res == true)
+
+  end
+  
+  test "json_val_alert_exeption_day0229" do
+
+    mc = MailrsController.new
+
+    res = mc.json_val_alert({
+        sendingAtYe:'1999',sendingAtMo:'2',sendingAtDa:'29'
+      })
+    assert(res == true)
+
+    res = mc.json_val_alert({
+        sendingAtYe:'1996',sendingAtMo:'2',sendingAtDa:'29'
+      })
+    assert(res == false)
+    
+    res = mc.json_val_alert({
+        sendingAtYe:'2000',sendingAtMo:'2',sendingAtDa:'29'
+      })
+    assert(res == false)
+    
+    res = mc.json_val_alert({
+        sendingAtYe:'1900',sendingAtMo:'2',sendingAtDa:'29'
+      })
+    assert(res == true)
+
+  end
+
+
+
 end
